1792f8feac2176a1fdcb5e948f8fb455f89008c8
tacnoma <ctchnm.geo@gmail.com>
2017-09-23 00:49:01 +0900

    Add index for voyager

--- App.js
+++ App.js
@@ -5,6 +5,26 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+            <div class="ly-wrap">
+
+            <a href="introduction.html">
+            <section>
+            <div class="ly-section-03">
+            <h1><img src="img/logo.png" alt="" width="187"></h1>
+            <p><span>Planetarhythm</span></p>
+            <p class="md-note-01">※当コンテンツ『VOY∀GER』は<br>5/6までの限定公開です。</p>
+            <p class="md-note-01">※スマートフォンに<br>最適化しています。</p>
+            <p>（画面をタップして次へ）</p>
+            <!--/.ly-section-03 --></div>
+            </section>
+            </a>
+
+            <!--/.ly-wrap --></div>
+
+            <div class="ly-foot">
+            <p><small>&copy;Planetarhythm: Towards the Nature of Planets</small></p>
+            <!--/.ly-foot --></div>
+
         <Text>Hello World!</Text>
         <Text>This is tacnoma`s Tranining React Native app!</Text>
       </View>
